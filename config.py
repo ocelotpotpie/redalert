faa99b56d0e22100b67eb0f6ae45549aac2b510c
Mike Savoie <savoie44@gmail.com>
2013-04-11 17:56:53 -0300

    Add info

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # make a new app here: https://dev.twitter.com/apps/new
 # fill in all the data :)
+# be sure you have "Read, Write and Access direct messages" enabled
 
 OAUTH_TOKEN = ""
 OAUTH_SECRET = ""

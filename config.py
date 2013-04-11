1b146adc4e25fcf3ce5102922bd6b2120d99c24b
Mike Savoie <savoie44@gmail.com>
2013-04-11 16:41:19 -0400

    Generic config.

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+# make a new app here: https://dev.twitter.com/apps/new
+# fill in all the data :)
+
 OAUTH_TOKEN = ""
 OAUTH_SECRET = ""
 CONSUMER_KEY = ""
 CONSUMER_SECRET = ""
-mcip = "mau5ville.com"
+mcip = "yourserver.com"
 port = 25565

99d06fcdf49c053da6923d0d0bd7b0d73fcc5c40
Mike Savoie <savoie44@gmail.com>
2013-04-11 17:50:06 -0400

    Various changes

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-redalert
-========
+#redalert
+Tweet if your minecraft server is down, optionally DM you.
 
-Tweet if your minecraft server is down, optionally DM
+## Usage
+    Edit config.py to your liking
+    chmod a+x redalert.py
+    python -B redalert.py
+
+## Making the twitter app
+
+* Sign into [the twitter app portal](https://dev.twitter.com/apps/new) and make a new app, call it whatever you want.
+* Generate yourself an OAuth token.
+* Fill in all the proper info in config.py
+* Make sure you have "Read, Write and Access direct messages" enabled
--- redalert.py
+++ redalert.py
@@ -14,13 +14,13 @@ from config import *
 
 # Setup
 magic = "\xFE"
-time = strftime("%H:%M:%S")
 t = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
 
 
 while 1:
     # Determine whether the server is up or down
-    try:
+    try:      
+        time = strftime("%H:%M:%S")
     	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((mcip, port))
         s.send(magic)
@@ -31,4 +31,4 @@ while 1:
     except Exception, e:
         # assume server is down
         print str(time) + " > " + str(e)
-    sleep(60)
+    sleep(300) # 5 minutes

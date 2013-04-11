3c92ebe89223e50fc7b9a33f083c6fe82c0cec70
Mike Savoie <savoie44@gmail.com>
2013-04-11 16:21:34 -0400

    Fix stupid socket bug

--- redalert.py
+++ redalert.py
@@ -21,15 +21,16 @@ port = 25565
 magic = "\xFE"
 time = strftime("%H:%M:%S")
 t = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 while 1:
     # Determine whether the server is up or down
     try:
+    	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((mcip, port))
         s.send(magic)
         data = s.recv(1024)
-    	s.close()
+        s.close()
         print data
     except Exception, e:
         print e

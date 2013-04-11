1e34b2585daa02376edc6a6402f557600a3abe65
Mike Savoie <savoie44@gmail.com>
2013-04-11 16:44:16 -0400

    Generate config

--- config.py
+++
@@ -1,9 +0,0 @@
-# make a new app here: https://dev.twitter.com/apps/new
-# fill in all the data :)
-
-OAUTH_TOKEN = ""
-OAUTH_SECRET = ""
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-mcip = "yourserver.com"
-port = 25565

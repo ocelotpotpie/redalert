a8d70cef881b99d2ec33cc3b0d32503c9228e8bf
Mike Savoie <savoie44@gmail.com>
2013-04-11 16:50:09 -0400

    Revert "Generate config"
    This reverts commit 1e34b2585daa02376edc6a6402f557600a3abe65.

--- redalert.py
+++ redalert.py
@@ -13,31 +13,15 @@ from config import *
 # Add your stuff in config.py
 
 # Setup
-
-if not os.path.exists('config.py'):
-    open('config.py', 'w').write(inspect.cleandoc(
-        r'''
-        # make a new app here: https://dev.twitter.com/apps/new
-        # fill in all the data :)
-
-        OAUTH_TOKEN = ""
-        OAUTH_SECRET = ""
-        CONSUMER_KEY = ""
-        CONSUMER_SECRET = ""
-        mcip = "yourserver.com"
-        port = 25565
-        ''') + '\n')
-
 magic = "\xFE"
 time = strftime("%H:%M:%S")
 t = Twitter(auth=OAuth(OAUTH_TOKEN, OAUTH_SECRET, CONSUMER_KEY, CONSUMER_SECRET))
 
 
-
 while 1:
     # Determine whether the server is up or down
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((mcip, port))
         s.send(magic)
         data = s.recv(1024)
